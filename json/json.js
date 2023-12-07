3c0b9845d7664a2546e5986527b2e93c114577ce
Prashant Sambhaji Mane <prashant.mane@in.techwave.net>
2023-12-07 14:33:17 +0530

    Json and string methods

--- json/json.js
+++ json/json.js
@@ -1,3 +1,8 @@
+// Json is a format for storing and trasporting data
+// data interchange formats
+// data in key/value pairs
+// 
+
 const obj = {name: "John", age: 30, city: "New York"};
 console.log(obj);
 const myobj = JSON.stringify(obj);
